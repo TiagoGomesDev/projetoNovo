0b003ed7c318eef0051087f62737b36a4aaf148b
TiagoGomesDev <rebeca.massoterapia01@gmail.com>
2021-06-01 10:27:04 -0300

    first commit

---
+++ src/components/Tutorial.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Tutorial = () => {
+  
+  return (
+    <div >
+      
+    </div>
+  );
+};
+
+export default Tutorial;
---
+++ src/components/TutorialsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TutorialList = () => {
+  
+  return (
+    <div >
+      
+    </div>
+  );
+};
+
+export default TutorialList;
